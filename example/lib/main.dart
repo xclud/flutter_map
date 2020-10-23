7aa05c95fd15d72b262b12af55de6733382118be
Mahdi <mahdi3466@yahoo.com>
2020-10-23 12:46:59 +0330

    Fixes a minor bug in the example project.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -84,7 +84,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 final url =
                     'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
 
-                return CachedNetworkImage(imageUrl: url);
+                return CachedNetworkImage(
+                  imageUrl: url,
+                  fit: BoxFit.cover,
+                );
               },
             ),
             Center(
