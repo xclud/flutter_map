79dd9965424abc2d9b45ed6815399a9fa64f6d05
Mahdi <mkfard@outlook.com>
2021-05-26 14:17:58 +0430

    Updated Github Pages.

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -10,9 +10,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "862b259cd9c0872aa8761f9a9076a735",
-"/": "862b259cd9c0872aa8761f9a9076a735",
-"main.dart.js": "d6c754dba063d6c5ea79964bae01cddd",
+"index.html": "acb3cc444ec2161f0086f0d76cb3514b",
+"/": "acb3cc444ec2161f0086f0d76cb3514b",
+"main.dart.js": "ab831a921ac1140b5fc6f579295637dd",
 "manifest.json": "09a21da4e4781dfe9bc2a495d86436fd",
 "version.json": "4b6db237b3514a88107a422469adfb0f"
 };
