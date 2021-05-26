7b8af23ffc12bc9d968268fad4d2708db359d89b
Mahdi <mkfard@outlook.com>
2021-05-26 14:12:45 +0430

    Add mouse wheel gesture in the example project.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:latlng/latlng.dart';
 import 'package:map/map.dart';
@@ -127,31 +128,40 @@ class _MyHomePageState extends State<MyHomePage> {
               print('${clicked.dx}, ${clicked.dy}');
               print('${details.localPosition.dx}, ${details.localPosition.dy}');
             },
-            child: Stack(
-              children: [
-                Map(
-                  controller: controller,
-                  builder: (context, x, y, z) {
-                    //Legal notice: This url is only used for demo and educational purposes. You need a license key for production use.
+            child: Listener(
+              onPointerSignal: (event) {
+                if (event is PointerScrollEvent) {
+                  final delta = event.scrollDelta;
 
-                    //Google Maps
-                    final url =
-                        'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
+                  controller.zoom -= delta.dy / 1000.0;
+                }
+              },
+              child: Stack(
+                children: [
+                  Map(
+                    controller: controller,
+                    builder: (context, x, y, z) {
+                      //Legal notice: This url is only used for demo and educational purposes. You need a license key for production use.
 
-                    //Mapbox Streets
-                    // final url =
-                    //     'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/$z/$x/$y?access_token=YOUR_MAPBOX_ACCESS_TOKEN';
+                      //Google Maps
+                      final url =
+                          'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
 
-                    return CachedNetworkImage(
-                      imageUrl: url,
-                      fit: BoxFit.cover,
-                    );
-                  },
-                ),
-                homeMarkerWidget,
-                ...markerWidgets,
-                centerMarkerWidget,
-              ],
+                      //Mapbox Streets
+                      // final url =
+                      //     'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/$z/$x/$y?access_token=YOUR_MAPBOX_ACCESS_TOKEN';
+
+                      return CachedNetworkImage(
+                        imageUrl: url,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                  homeMarkerWidget,
+                  ...markerWidgets,
+                  centerMarkerWidget,
+                ],
+              ),
             ),
           );
         },
